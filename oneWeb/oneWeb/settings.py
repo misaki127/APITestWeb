d307545c7592613c53b8e73a6caffebf82263959
misaki127 <33180312+misaki127@users.noreply.github.com>
2021-01-15 18:28:55 +0800

    Update settings.py

--- oneWeb/oneWeb/settings.py
+++ oneWeb/oneWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pbw1ve081t8o^=emvk3di83*$^##1zyku-a!oglxlsf5nab$+m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
